7dc3cec2f270b39fbbd04a9973f09a7bcf054bbb
Connor Lee <clee@caltech.edu>
2015-10-20 00:58:09 +0100

    finalized website

--- js/uav.js
+++ js/uav.js
@@ -1,65 +1,81 @@
 $("#menu-close").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+$('a[href*=#]:not([href=#top],[href=#])').hover(function() {
+    $("#uav").css({
+        '-ms-transform': 'rotate(90deg)',
+        '-webkit-transform': 'rotate(90deg)',
+        'transform': 'rotate(90deg)',
+        '-webkit-transition': '.3s ease-in',
+        '-moz-transition': '.3s ease-in',
+        '-o-transition': '.3s ease-in',
+        'transition': '.3s ease-in',
     });
-
-    // Opens the sidebar menu
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
+}, function() {
+    $("#uav").css({
+        '-ms-transform': 'rotate(0deg)',
+        '-webkit-transform': 'rotate(0deg)',
+        'transform': 'rotate(0deg)',
+        '-webkit-transition': '.3s ease-in',
+        '-moz-transition': '.3s ease-in',
+        '-o-transition': '.3s ease-in',
+        'transition': '.3s ease-in',
     });
+});
 
-    $('a[href*=#]:not([href=#top],[href=#])').hover(function(){ 
-            $("#uav").css({
-               '-ms-transform': 'rotate(90deg)',
-                '-webkit-transform': 'rotate(90deg)', 
-                'transform': 'rotate(90deg)', 
-                '-webkit-transition': '.3s ease-in',
-                '-moz-transition': '.3s ease-in',
-                '-o-transition': '.3s ease-in',
-                'transition': '.3s ease-in',
-            });
-        }, function(){
-            $("#uav").css({
-               '-ms-transform': 'rotate(0deg)',
-                '-webkit-transform': 'rotate(0deg)', 
-                'transform': 'rotate(0deg)', 
-                '-webkit-transition': '.3s ease-in',
-                '-moz-transition': '.3s ease-in',
-                '-o-transition': '.3s ease-in',
-                'transition': '.3s ease-in',
-            });
-        });
-        
-        $('a[href*=#]:not([href=#])').click(function() {
-            $('#uavBox')
-            .transition({y: '+=700'}, 700, 'ease');
-            setTimeout(function(){
-                $('#uavBox').transition({rotate: '-90deg'}, 200, 'ease');
-                $('#uavBox').transition({y: '-=700'}, 500, 'ease');
-                $('#uavBox').transition({rotate: '0deg'}, 200, 'ease');
-            }, 700);
-        });
+$('a[href*=#]:not([href=#])').click(function() {
+    $('#uavBox')
+        .transition({
+            y: '+=700'
+        }, 700, 'ease');
+    setTimeout(function() {
+        $('#uavBox').transition({
+            rotate: '-90deg'
+        }, 200, 'ease');
+        $('#uavBox').transition({
+            y: '-=700'
+        }, 500, 'ease');
+        $('#uavBox').transition({
+            rotate: '0deg'
+        }, 200, 'ease');
+    }, 700);
+});
 
 
-    // Scrolls to the selected menu item on the page
-   $(function() {
+// Scrolls to the selected menu item on the page
+$(function() {
 
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
-                var target = $(this.hash);
+            var target = $(this.hash);
 
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
- 
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
-                    return false;
-                }
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+
+                return false;
             }
-        });
-        
+        }
     });
+
+});
+
+$('#message').on('keyup', function() {
+    $("#characterLeft").text($(this).val().length + '/' + 4096);
+});
+
+$('#me100-overlay').on('click', function() {
+    $('#me100-2014-modal').modal();
+});
